c227cee1d2f4523553cf1b831237c781a73cde10
Piyush Yadav <piyushya012@gmail.com>
2022-09-11 15:36:00 +0530

    initial project

---
+++ app/routes.py
@@ -0,0 +1,31 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Piyush'}
+    posts = [
+        {
+            'author': {'username': 'Rajat'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Annu'},
+            'body': 'The Boys movie was so cool!'
+        },
+        {
+            'author': {'username': 'Abhi'},
+            'body': 'Happy birthday to me'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
